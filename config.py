a9a258553fcc5c7b1e6580151643bb879cf06362
Erik <eekihenriksson@gmail.com>
2025-05-06 20:05:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# Base paths with placeholders for language code
-STATS_PATH_TEMPLATE = "/scratch/project_462000353/tlundber/umap-embeddings/register-distribution-results-th-optimised/{}_embeds.tsv"
-DATA_PATH_TEMPLATE = "/scratch/project_462000353/tlundber/umap-embeddings/data/model_embeds/cleaned/bge-m3-fold-6/th-optimised/{}_embeds.tsv"
-FILTERED_BY_MEDIAN_AND_STD_TEMPLATE = (
-    "1_filtered_by_median_and_std/{}_embeds_filtered.tsv"
-)
+STATS_PATH = "/scratch/project_462000353/tlundber/umap-embeddings/register-distribution-results-th-optimised"
+RAW_DATA_PATH = "/scratch/project_462000353/tlundber/umap-embeddings/data/model_embeds/cleaned/bge-m3-fold-6/th-optimised"
+FILTERED_BY_MEDIAN_AND_STD_PATH = "filtered_by_median_and_std"
+FILTERED_BY_MIN_LENGTH_PATH = "filtered_by_min_length"
+
+MIN_TEXT_LENGTH = 10  # You can adjust this value as needed
